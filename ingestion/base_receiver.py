df798b1eea76f4e900d763214d86d7b4905d27fe
heesu <heesu12200@gmail.com>
2025-07-24 11:54:24 -0500

    Initial commit

---
+++ ingestion/base_receiver.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class BaseReceiver(ABC):
+    """
+    Base class for log ingestion modules.
+
+    This abstract class defines a common interface and shared functionality
+    for different types of log receivers (e.g., TCP, Kafka).
+
+    """
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        """Start receiving data."""
+        pass
+
+    @abstractmethod
+    def stop(self):
+        """Stop receiving data."""
+        pass
+
+    @abstractmethod
+    def handle_message(self, message: str):
+        """Callback for handling each received message."""
+        pass
